813c79bb4471884648e4729ed33e6bcd9be605d9
Boris-Colin <125480120+Boris-Colin@users.noreply.github.com>
2024-10-27 12:14:09 +0100

    csv ready to be used in excel

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ path = "C:\\Users\\1thom\\OneDrive\\Bureau\\Ultimate_Budget_Empty.xlsx"
 path2 = "C:\\Users\\1thom\\Downloads\\export-operations-27-10-2024_08-51-13.csv"
 
 df = pd.read_csv(path2, sep=';')
+df_copy = df.drop(columns=['dateVal', 'categoryParent', 'comment', 'accountNum', 'accountLabel', 'accountbalance'])
+
+df_copy['dateOp'] = pd.to_datetime(df_copy['dateOp'], errors='coerce')
+# Remove commas and convert 'amount' to float
+df_copy['amount'] = df_copy['amount'].str.replace(' ', '').str.replace(',', '.').astype(float)
+
+# Add a 'year' column
+df_copy['year'] = df_copy['dateOp'].dt.year
+# Add 'month' column
+df_copy['month'] = df_copy['dateOp'].dt.month
+
+# Group by 'year' and 'month' and get the average sales
+monthly_ex = df_copy.groupby(['year', 'month'])['amount'].mean()
+
+print(monthly_ex)
+
 
 wb = openpyxl.load_workbook(path)
 
@@ -36,10 +52,10 @@ print(max_col)
 #c4 = sheet['K10']
 #c4.value = "RAI"
 
-print(df.describe())
-print(df.info())
-print(df.head())
-wb.save(path)
+
+print(df_copy.info())
+print(df_copy.head())
+#wb.save(path)
 
 
 
